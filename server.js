0e8f862dbe99bf1a106513a368de04ba93e9f884
Cassidy Whitehead <whi21049@byui.edu>
2025-10-08 20:06:06 -0600

    MVC implementation

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /************************
  * View Engine and Templates
@@ -24,9 +25,12 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route!
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+
+// Old inde route rendered directly in the route
+// app.get("/", function(req, res){
+//   res.render("index", {title: "Home"})
+// })
 
 /************************
  * Local Server Information
