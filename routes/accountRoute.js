e1760f75ad03d89e8c4d6112584e263fc7439b88
Cassidy Whitehead <whi21049@byui.edu>
2025-10-23 16:07:57 -0600

    Finished registration

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,4 +10,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route to build the registration
 router.get("/registration", utilities.handleErrors(accountController.buildRegistration))
 
+// Route to enable registration route
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
 module.exports = router
