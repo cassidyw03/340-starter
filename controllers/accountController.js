6623562fef183eb38792da1a1cb5d31600f8d9cc
Cassidy Whitehead <whi21049@byui.edu>
2025-10-24 20:42:31 -0600

    server-side data validation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -9,6 +9,8 @@ async function buildLogin(req, res, next) {
   res.render("account/login", {
     title: "Login",
     nav,
+    notice: req.flash("notice"),
+    errors: null
   })
 }
 
@@ -41,17 +43,24 @@ async function registerAccount(req, res) {
   if (regResult) {
     req.flash(
       "notice",
-      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
-    )
-    res.status(201).render("account/login", {
-      title: "Login",
-      nav,
-    })
+      `Congratulations, you\'re registered ${account_firstname}! Please log in.`)
+      console.log("registerAccount: regResult true - redirecting to /account/login")
+    // res.status(201).redirect("account/login")
+    return res.redirect("/account/login")
+  
+    // res.status(201).redirect("account/login", {
+    //   title: "Registration Successful",
+    //   nav,
+    //   message: `Welcome, ${account_firstname}! Your account `,
+    //   notice: req.flash("notice")
+    // })
   } else {
     req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
+    res.status(501).render("account/registration", {
       title: "Registration",
       nav,
+      notice: req.flash("notice"),
+      errors: null
     })
   }
 }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -10,7 +11,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route to build the registration
 router.get("/registration", utilities.handleErrors(accountController.buildRegistration))
 
-// Route to enable registration route
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router
