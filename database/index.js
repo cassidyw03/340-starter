569635c0443cdc2d2d52f9e36d2902da48bc8b70
Cassidy Whitehead <whi21049@byui.edu>
2025-12-18 13:42:06 -0700

    not delopying on render

--- database/index.js
+++ database/index.js
@@ -1,37 +1,66 @@
+// const { Pool } = require("pg")
+// require("dotenv").config()
+// /* *************************************************
+//  * Connection Pool
+//  * SSL Object needed for local testing of app
+//  * But will cause problems in production environment
+//  * If - else will make determination which to use
+//  * ************************************************* */
+// let pool
+// if (process.env.NODE_ENV == "development") {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+// })
+
+// // Added for troubleshooting queries
+// // during development
+// module.exports = {
+//   async query(text, params) {
+//     try {
+//       const res = await pool.query(text, params)
+//       console.log("executed query", { text })
+//       return res
+//     } catch (error) {
+//       console.error("error in query", { text })
+//       throw error
+//     }
+//   },
+// }
+// } else {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   })
+//   module.exports = pool
+// }
+
+// HMMM something is breaking
+
 const { Pool } = require("pg")
 require("dotenv").config()
-/* *************************************************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * ************************************************* */
-let pool
-if (process.env.NODE_ENV == "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+
+const isProduction = process.env.NODE_ENV === "production"
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 })
 
-// Added for troubleshooting queries
-// during development
-module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
+// Optional query logging (safe)
+const originalQuery = pool.query
+pool.query = async function (text, params) {
+  try {
+    const res = await originalQuery.call(this, text, params)
+    if (!isProduction) {
       console.log("executed query", { text })
-      return res
-    } catch (error) {
-      console.error("error in query", { text })
-      throw error
     }
-  },
+    return res
+  } catch (error) {
+    console.error("error in query", { text })
+    throw error
+  }
 }
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
-}
+
+module.exports = pool
--- server.js
+++ server.js
@@ -116,6 +116,10 @@ const host = process.env.HOST
 /************************
  * Log statement to confirm server operation
  *************************/
+// app.listen(port, () => {
+//   console.log(`app listening on ${host}:${port}`)
+// })
+
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on ${port}`)
 })
